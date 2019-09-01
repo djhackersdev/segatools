eccb5557d018a6616027e094e66abb497ec0fa9a
Tau <tau@nowhere>
2019-09-01 11:48:55 -0400

    util/async.c: Include NTSTATUS
    Fixes non-PCH build.

--- util/async.c
+++ util/async.c
@@ -1,4 +1,8 @@
+/* NTSTATUS chicanery. See precompiled.h */
+#define WIN32_NO_STATUS
 #include <windows.h>
+#undef WIN32_NO_STATUS
+#include <winternl.h>
 #include <ntstatus.h>
 
 #include <assert.h>
