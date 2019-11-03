a5a42c3326c5a594dd5c29c09eff6371de1b52d4
Tau <tau@nowhere>
2019-11-03 16:14:26 -0500

    idzhook/zinput.c: Clean up some clutter

--- idzhook/zinput.c
+++ idzhook/zinput.c
@@ -19,56 +19,58 @@ HRESULT WINAPI hook_DirectInput8Create(
          LPVOID *ppvOut,
          LPUNKNOWN punkOuter);
 
-static unsigned long WINAPI my_AddRef(IUnknown *self);
+static unsigned long WINAPI hook_AddRef(IUnknown *self);
 
-static unsigned long WINAPI my_Release(IUnknown *self);
+static unsigned long WINAPI hook_Release(IUnknown *self);
 
-static HRESULT WINAPI my_CreateDevice(
+static HRESULT WINAPI hook_CreateDevice(
         IDirectInput8W *self,
         REFGUID rguid,
         LPDIRECTINPUTDEVICE8W * lplpDirectInputDevice,
         LPUNKNOWN pUnkOuter);
 
-static HRESULT WINAPI my_EnumDevices(
+static HRESULT WINAPI hook_EnumDevices(
         IDirectInput8W *self,
         DWORD dwDevType,
         LPDIENUMDEVICESCALLBACKW lpCallback,
         LPVOID pvRef,
         DWORD dwFlags);
 
-static HRESULT WINAPI my_SetDataFormat(
+static HRESULT WINAPI hook_SetDataFormat(
         IDirectInputDevice8W *self,
         LPCDIDATAFORMAT lpdf);
 
-static HRESULT WINAPI my_SetCooperativeLevel(
+static HRESULT WINAPI hook_SetCooperativeLevel(
         IDirectInputDevice8W *self,
         HWND hwnd,
         DWORD flags);
 
-static HRESULT WINAPI my_Acquire(IDirectInputDevice8W *self);
+static HRESULT WINAPI hook_Acquire(IDirectInputDevice8W *self);
 
-static HRESULT WINAPI my_GetDeviceState(
+static HRESULT WINAPI hook_Unacquire(IDirectInputDevice8W *self);
+
+static HRESULT WINAPI hook_GetDeviceState(
         IDirectInputDevice8W *self,
         DWORD cbData,
         LPVOID lpvData);
 
 static const IDirectInput8WVtbl api_vtbl = {
-    .AddRef             = (void *) my_AddRef,
-    .Release            = (void *) my_Release,
-    .CreateDevice       = my_CreateDevice,
-    .EnumDevices        = my_EnumDevices,
+    .AddRef             = (void *) hook_AddRef,
+    .Release            = (void *) hook_Release,
+    .CreateDevice       = hook_CreateDevice,
+    .EnumDevices        = hook_EnumDevices,
 };
 
 static const IDirectInput8W api = { (void *) &api_vtbl };
 
 static const IDirectInputDevice8WVtbl dev_vtbl = {
-    .AddRef             = (void *) my_AddRef,
-    .Release            = (void *) my_Release,
-    .SetDataFormat      = my_SetDataFormat,
-    .SetCooperativeLevel= my_SetCooperativeLevel,
-    .Acquire            = my_Acquire,
-    .Unacquire          = my_Acquire, // not a c&p error
-    .GetDeviceState     = my_GetDeviceState,
+    .AddRef             = (void *) hook_AddRef,
+    .Release            = (void *) hook_Release,
+    .SetDataFormat      = hook_SetDataFormat,
+    .SetCooperativeLevel= hook_SetCooperativeLevel,
+    .Acquire            = hook_Acquire,
+    .Unacquire          = hook_Unacquire,
+    .GetDeviceState     = hook_GetDeviceState,
 };
 
 static const IDirectInputDevice8W dev = { (void *) &dev_vtbl };
@@ -110,65 +112,70 @@ HRESULT WINAPI hook_DirectInput8Create(
     return S_OK;
 }
 
-static unsigned long WINAPI my_AddRef(IUnknown *self)
+static unsigned long WINAPI hook_AddRef(IUnknown *self)
 {
     return 1;
 }
 
-static unsigned long WINAPI my_Release(IUnknown *self)
+static unsigned long WINAPI hook_Release(IUnknown *self)
 {
     return 1;
 }
 
-static HRESULT WINAPI my_CreateDevice(
+static HRESULT WINAPI hook_CreateDevice(
         IDirectInput8W *self,
         REFGUID rguid,
         LPDIRECTINPUTDEVICE8W *lplpDirectInputDevice,
         LPUNKNOWN pUnkOuter)
 {
-    dprintf("%s\n", __func__);
+    dprintf("ZInput: %s\n", __func__);
     *lplpDirectInputDevice = (void *) &dev;
 
     return S_OK;
 }
 
-static HRESULT WINAPI my_EnumDevices(
+static HRESULT WINAPI hook_EnumDevices(
         IDirectInput8W *self,
         DWORD dwDevType,
         LPDIENUMDEVICESCALLBACKW lpCallback,
         LPVOID pvRef,
         DWORD dwFlags)
 {
-    dprintf("%s\n", __func__);
+    dprintf("ZInput: %s\n", __func__);
 
     return S_OK;
 }
 
-static HRESULT WINAPI my_SetDataFormat(
+static HRESULT WINAPI hook_SetDataFormat(
         IDirectInputDevice8W *self,
         LPCDIDATAFORMAT lpdf)
 {
-    dprintf("%s\n", __func__);
+    dprintf("ZInput: %s\n", __func__);
 
     return S_OK;
 }
 
-static HRESULT WINAPI my_SetCooperativeLevel(
+static HRESULT WINAPI hook_SetCooperativeLevel(
         IDirectInputDevice8W *self,
         HWND hwnd,
         DWORD flags)
 {
-    dprintf("%s\n", __func__);
+    dprintf("ZInput: %s\n", __func__);
 
     return S_OK;
 }
 
-static HRESULT WINAPI my_Acquire(IDirectInputDevice8W *self)
+static HRESULT WINAPI hook_Acquire(IDirectInputDevice8W *self)
 {
     return S_OK;
 }
 
-static HRESULT WINAPI my_GetDeviceState(
+static HRESULT WINAPI hook_Unacquire(IDirectInputDevice8W *self)
+{
+    return S_OK;
+}
+
+static HRESULT WINAPI hook_GetDeviceState(
         IDirectInputDevice8W *self,
         DWORD cbData,
         LPVOID lpvData)
