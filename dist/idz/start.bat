862dcd224e5b0c8a79a46678b8aedb578bc29036
1b9b20430b7b392400d20de45cf44dc3420cbaef <1b9b20430b7b392400d20de45cf44dc3420cbaef@redacted>
2019-11-08 10:54:59 +0800

    Update start.bat make it convenient for everyone to run it as Administrator

--- dist/idz/start.bat
+++ dist/idz/start.bat
@@ -1,5 +1,7 @@
 @echo off
 
+pushd %~dp0
+
 .\inject.exe -k .\idzhook.dll .\InitialD0_DX11_Nu.exe
 .\inject.exe -d -k .\idzhook.dll .\amdaemon.exe -c configDHCP_Final_Common.json configDHCP_Final_JP.json configDHCP_Final_JP_ST1.json configDHCP_Final_JP_ST2.json configDHCP_Final_EX.json configDHCP_Final_EX_ST1.json configDHCP_Final_EX_ST2.json
 
