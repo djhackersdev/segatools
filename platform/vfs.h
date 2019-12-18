0d8dd05cebd70fc2dbc7d301b6669b6241bf7d6f
Shiz <hi@shiz.me>
2019-12-18 20:51:09 +0100

    platform/vfs.c: add option path redirection
    Optional for now, since not all games or configurations may need it.
    It may be better eventually to refactor hooklib/path.c to allow for
    static path remapping, to minimize boilerplate code around the functions.

--- platform/vfs.h
+++ platform/vfs.h
@@ -9,6 +9,7 @@ struct vfs_config {
     bool enable;
     wchar_t amfs[MAX_PATH];
     wchar_t appdata[MAX_PATH];
+    wchar_t option[MAX_PATH];
 };
 
 HRESULT vfs_hook_init(const struct vfs_config *config);
