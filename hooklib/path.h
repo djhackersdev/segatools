00819a2721847c2fa3d907fd2391181863fac491
Matt Bilker <me@mbilker.us>
2020-02-13 03:14:59 +0000

    platform/vfs.c: allow nthome and option path hooks without ending back-slash
    - This actually fixed option loading.
    
    - Ongeki uses forward-slashes for Y:/SDDT/log

--- hooklib/path.h
+++ hooklib/path.h
@@ -2,6 +2,7 @@
 
 #include <windows.h>
 
+#include <stdbool.h>
 #include <stddef.h>
 
 typedef HRESULT (*path_hook_t)(
@@ -11,3 +12,8 @@ typedef HRESULT (*path_hook_t)(
 
 HRESULT path_hook_push(path_hook_t hook);
 int path_compare_w(const wchar_t *string1, const wchar_t *string2, size_t count);
+
+static inline bool path_is_separator_w(wchar_t c)
+{
+    return c == L'\\' || c == L'/';
+}
