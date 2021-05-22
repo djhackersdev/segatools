81f90981b46d272328e4968ae8223b07e1cd71c9
Tau <tau@nowhere>
2021-05-22 12:19:28 -0400

    Delete draft debris that was never used

--- hooklib/dll.h
+++ hooklib/dll.h
@@ -5,12 +5,6 @@
 
 #include "hook/table.h"
 
-struct dll_symbol {
-    void *ptr;
-    const char *name;
-    uint16_t ordinal;
-};
-
 HRESULT dll_hook_push(
         HMODULE redir_mod,
         const wchar_t *name,
