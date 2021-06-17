031ccb154bd424af8d4c1d4875d842beaf08086f
6cb1008cabfc0d4485696f6df7431a5e27604e1b <6cb1008cabfc0d4485696f6df7431a5e27604e1b@redacted>
2021-06-17 04:13:50 +0200

    dvd: move thread safety comment into header file

--- hooklib/dvd.c
+++ hooklib/dvd.c
@@ -46,9 +46,6 @@ void dvd_hook_init(const struct dvd_config *cfg, HINSTANCE self)
         return;
     }
 
-    /* Init is not thread safe because API hook init is not thread safe blah
-        blah blah you know the drill by now. */
-
     if (dvd_hook_initted) {
         return;
     }
--- hooklib/dvd.h
+++ hooklib/dvd.h
@@ -8,4 +8,7 @@ struct dvd_config {
     bool enable;
 };
 
+/* Init is not thread safe because API hook init is not thread safe blah
+    blah blah you know the drill by now. */
+
 void dvd_hook_init(const struct dvd_config *cfg, HINSTANCE self);
